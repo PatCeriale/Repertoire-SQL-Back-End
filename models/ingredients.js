446ae059df70ee1cae280e5dca2cf7981d3bcc93
Patrick Ceriale <patceriale@gmail.com>
2021-01-28 16:42:21 -0800

    created ingredients model

---
+++ models/ingredients.js
@@ -0,0 +1,32 @@
+module.exports = function (sequelize, DataTypes) {
+  var Ingredients = sequelize.define("Ingredients", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    createdAt: {
+      type: "TIMESTAMP",
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+      allowNull: false,
+    },
+  });
+
+  Ingredients.associate = function (models) {
+    Ingredients.belongsTo(models.Recipe, {
+      foreignKey: {
+        //TODO: Change back to false before deployment
+        allowNull: true,
+      },
+    });
+  };
+  return Ingredients;
+};
