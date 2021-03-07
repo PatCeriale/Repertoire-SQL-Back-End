5c4cd1c173c46155714404942736de0199ebc284
Patrick Ceriale <patceriale@gmail.com>
2021-03-06 16:43:04 -0800

    got routes to work through server.js

--- server.js
+++ server.js
@@ -16,6 +16,46 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to Repertoire's back end." });
 });
 
+app.post("/api/newrecipe", function (req, res) {
+  db.Recipe.create({
+    title: req.body.title,
+    // description: req.body.description,
+    // prepTime: req.body.preptime,
+    // cookTime: req.body.cooktime,
+    // servingSize: req.body.servingSize,
+    // course: req.body.course,
+    // image: req.body.image,
+  })
+    .then(function (dbRecipe) {
+      // res.redirect("/recipe/:id");
+      res.json(dbRecipe);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+app.get("/api/recipes", function (req, res) {
+  db.Recipe.findAll({
+    // title: req.body.title,
+    // description: req.body.description,
+    // prepTime: req.body.preptime,
+    // cookTime: req.body.cooktime,
+    // servingSize: req.body.servingSize,
+    // course: req.body.course,
+    // image: req.body.image,
+  })
+    .then(function (dbRecipe) {
+      // res.redirect("/recipe/:id");
+      res.json(dbRecipe);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 db.sequelize.sync({ force: false }).then(function () {
   app.listen(PORT, function () {
     console.log("Listening for your recipe selection on PORT " + PORT);
